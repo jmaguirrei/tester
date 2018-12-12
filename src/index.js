c89aa8645ae7c76b545606ce63af214b58fb43d6
José Manuel Aguirre <jmaguirrei@gmail.com>
2018-12-12 12:21:16 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -8,27 +8,17 @@ const tests = {};
 
 tests.init = function (homePath) {
 
-  console.log("homePath", homePath);
   console.log('Test Runner inited ...');
 
   walkDirectory(homePath)
   .then(testingFilePaths => {
 
     tests.units = testingFilePaths.map(filePath => {
-      console.log("filePath", filePath);
-      // const subPath = filePath.substr(homePath.length, 10000);
-      // console.log("subPath", subPath);
-      // const relativePath = PATH_TO_TEST + subPath;
-      // const folderPath = subPath.split('__tests__')[0];
-      // const fileName = filePath.substr(relativePath.lastIndexOf('/') + 1, 1000);
+
       const folderPath = filePath.split('__tests__')[0];
-      console.log("folderPath", folderPath);
-      // console.log("folderPath", folderPath);
       const fileName = filePath.substr(filePath.lastIndexOf('/') + 1, 1000);
-      console.log("fileName", fileName);
-      // console.log("fileName", fileName);
       const testsContainerObject = require(filePath).default;
-      console.log("testsContainerObject", testsContainerObject);
+
       const runnables = Object.keys(testsContainerObject).map(key => {
         return {
           name: key,
